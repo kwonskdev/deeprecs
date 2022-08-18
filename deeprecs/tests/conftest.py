4d0ecc4ebd565c8cc1fb1324b29b530fbfd50e2d
kwonsk <nerochaos1@gmail.com>
2022-08-18 14:05:20 +0900

    feat: Make ml100k fixture
    - Make ml100k fixture.
    - test_ml100k uses ml100k fixture.

--- deeprecs/tests/conftest.py
+++ deeprecs/tests/conftest.py
@@ -1,6 +1,11 @@
 # pylint: disable=import-error, unused-variable
+import pandas as pd
 import pytest
+import torch
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
 
+from deeprecs.data.data import AEDataset
 from deeprecs.models.autorec import AutoEncoder
 
 
@@ -35,3 +40,22 @@ def autorec():
         return autoencoder
 
     return wrapper
+
+
+@pytest.fixture
+def ml100k():
+    """
+    pytest에서 모델 최저 성능 보장을 위해 사용할 MovieLens 100k data
+    """
+    ml = pd.read_csv("./data/ml-100k/ml-100k_pivot.csv", index_col=0)
+    ml = ml.fillna(0)
+    train, test = map(
+        AEDataset,
+        train_test_split(
+            torch.Tensor(ml.values), test_size=0.1, random_state=42
+        ),
+    )
+    train_loader = DataLoader(train, batch_size=32)
+    test_loader = DataLoader(test, batch_size=len(test))
+
+    return (train_loader, test_loader)
--- deeprecs/tests/model_test.py
+++ deeprecs/tests/model_test.py
@@ -1,19 +1,25 @@
 # pylint: disable=import-error
+from typing import Tuple
+
 import numpy as np
-import pandas as pd
 import pytest
 import pytest_lazyfixture
-import torch
-from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
 
-from deeprecs.data.data import AEDataset
 from deeprecs.models.base import BaseRecommender
 from deeprecs.utils.loss import RMSELoss
 
 
-@pytest.mark.parametrize("model", [pytest_lazyfixture.lazy_fixture("autorec")])
-def test_ml100k(model: BaseRecommender):
+@pytest.mark.parametrize(
+    "model, data",
+    [
+        (
+            pytest_lazyfixture.lazy_fixture("autorec"),
+            pytest_lazyfixture.lazy_fixture("ml100k"),
+        )
+    ],
+)
+def test_ml100k(model: BaseRecommender, data: Tuple[DataLoader, DataLoader]):
     """
     movielens 100k 데이터에 대해서 모델의 예측값이 잘 나오는지 확인하는 테스트함수
 
@@ -21,26 +27,17 @@ def test_ml100k(model: BaseRecommender):
     ----------
     model : BaseRecommender
         추천 모델
+    data : Tuple[DataLoader, DataLoader]
+        학습/실험 데이터셋
     """
     model = model(input_dim=1682, hidden_dim=128)
-
-    # TODO: 데이터도 fixture로 관리 / train, test까지
-    ml = pd.read_csv("./data/ml-100k/ml-100k_pivot.csv", index_col=0)
-    ml = ml.fillna(0)
-    train, test = map(
-        AEDataset,
-        train_test_split(
-            torch.Tensor(ml.values), test_size=0.1, random_state=42
-        ),
-    )
-    train_loader = DataLoader(train, batch_size=32)
-    test_loader = DataLoader(test, batch_size=len(test))
+    train_loader, test_loader = data
 
     model.fit(train_loader, epochs=1)
     pred = model.predict(test_loader)
     pred = np.clip(pred, 1, 5)
 
-    rmse = RMSELoss()(test.y, pred)
+    rmse = RMSELoss()(test_loader.dataset.y, pred)
     # https://paperswithcode.com/sota/collaborative-filtering-on-movielens-100k
     # 기준으로 가장 높은 rmse는 0.996
     assert rmse <= 1.15
